e56d24e404c412cf2136036c90e06726448d552a
evarayHEIG <eva.ray@heig-vd.ch>
2023-11-28 11:23:14 +0100

    Start MailContent implementation

--- code/Rafou2898_evarayHEIG/Client/src/main/java/ch/heig/dai/lab/smtp/MailContent.java
+++ code/Rafou2898_evarayHEIG/Client/src/main/java/ch/heig/dai/lab/smtp/MailContent.java
@@ -1,4 +1,34 @@
 package ch.heig.dai.lab.smtp;
 
 public class MailContent {
+
+    private final String DOMAIN;
+
+    public MailContent(String domain){
+        this.DOMAIN = domain;
+    }
+
+    String mailFrom(String sender){
+
+        // We only want the part of the string before "@"
+        int atIndex = sender.indexOf('@');
+        String address = sender.substring(0, atIndex);
+
+        return "MAIL FROM: <" + address + "@" + DOMAIN + ">";
+    }
+
+    String mailTo(String victim){
+
+        return "RCPT TO: <" + victim + ">";
+    }
+
+
+    String data(FileManager.Message message, String receiver){
+
+        String data, date, from;
+
+        date = java.time.LocalDateTime.now().toString() + "\n";
+
+        return null;
+    }
 }
