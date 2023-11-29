706d4ad0938df663d2e6af1849440117931f34ba
Rafou98 <95037521+Rafou28@users.noreply.github.com>
2023-11-29 12:41:17 +0100

    refactor: change in the code

--- code/Rafou2898_evarayHEIG/Client/src/main/java/ch/heig/dai/lab/smtp/MailContent.java
+++ code/Rafou2898_evarayHEIG/Client/src/main/java/ch/heig/dai/lab/smtp/MailContent.java
@@ -1,34 +1,51 @@
 package ch.heig.dai.lab.smtp;
 
+import javax.mail.internet.MimeUtility;
+import java.io.UnsupportedEncodingException;
+
 public class MailContent {
 
-    private final String DOMAIN;
+    //Pas sure que ce soit nécessaire
+    // private final String DOMAIN;
 
-    public MailContent(String domain){
-        this.DOMAIN = domain;
+    public MailContent(/*String domain*/){
+        /*this.DOMAIN = domain;*/
     }
 
-    String mailFrom(String sender){
-
-        // We only want the part of the string before "@"
-        int atIndex = sender.indexOf('@');
-        String address = sender.substring(0, atIndex);
-
-        return "MAIL FROM: <" + address + "@" + DOMAIN + ">";
+    String hello(String DOMAIN){
+        return "EHLO " + DOMAIN;
     }
 
-    String mailTo(String victim){
+    /**
+     * Used to send the mail to the victim
+     * @param sender the victim's mail
+     * @param MAIL_FROM true if you want to send RECTP TO or false if you want to send to
+     * @return the string to send to the server
+     */
+    String mailFrom(String sender, boolean MAIL_FROM){
+        return MAIL_FROM ? "MAIL FROM: <" + sender + ">" : "From: " + sender;
+    }
 
-        return "RCPT TO: <" + victim + ">";
+    /**
+     * Used to send the mail to the victim
+     * @param victim the victim's mail
+     * @param RCPT_TP true if you want to send RECTP TO or false if you want to send to
+     * @return the string to send to the server
+     */
+    String mailTo(String victim, boolean RCPT_TP){
+        return RCPT_TP ? "RCPT TO: <" + victim + ">" : "To: " + victim ;
     }
 
 
-    String data(FileManager.Message message, String receiver){
-
-        String data, date, from;
-
-        date = java.time.LocalDateTime.now().toString() + "\n";
-
-        return null;
+    String data( Message message) throws UnsupportedEncodingException {
+        return String.format(
+                "Date: %s\n" +
+                "Subject: %s\n" +
+                "\n" +
+                "%s\r\n" +
+                //alors ici le MimeUtility.encodeText permet d'encoder le sujet et le body en UTF-8 pour le mail sur le serveur
+                //mais du coup dans le terminal ça n'affiche pas bien les messages
+                // Rectification... ça fait quand meme de la merde...
+                ".\r\n", java.time.LocalDateTime.now().toString(),MimeUtility.encodeText( message.getSubject(), "UTF-8", "B"), message.getBody()/*MimeUtility.encodeText(message.getBody(), "UTF-8", "B")*/);
     }
 }
